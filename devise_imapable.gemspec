c0d9d74a4b62ab6ea6541aff6b315b14059ced01
Filipe Alves <filipe.alves@bithium.com>
2019-07-16 12:27:28 +0100

    CHG: Setup environment for Devise integration testing.

--- devise_imapable.gemspec
+++ devise_imapable.gemspec
@@ -38,8 +38,11 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'activemodel', '~>5.2.0'
   spec.add_dependency 'devise', '~> 4.6.0'
 
+  spec.add_development_dependency 'activerecord', '>= 5.2.3'
   spec.add_development_dependency 'bundler', '~> 2.0'
+  spec.add_development_dependency 'faker', '~> 1.9.6'
   spec.add_development_dependency 'pry', '~>0.12.2'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'sqlite3', '>= 1.4.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'faker'
+
+require 'sqlite3'
+require 'active_record'
+
+require 'devise'
 require 'devise_imapable'
 
 RSpec.configure do |config|
@@ -13,4 +19,60 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+# ===============================================================================
+# Helper Classes
+# ===============================================================================
+
+require 'rack/test'
+require 'action_controller/railtie'
+require 'active_record'
+require 'devise/rails/routes'
+require 'devise/rails/warden_compat'
+
+ActiveRecord::Base.establish_connection(adapter: :sqlite3, database: ':memory:')
+
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table(:users) do |t|
+      t.string :email,              null: false
+      t.string :encrypted_password, null: true
+      t.timestamps null: false
+    end
+  end
+
+end
+
+Devise.setup do |config|
+  require 'devise/orm/active_record'
+  config.secret_key = 'secret_key_base'
+end
+
+class TestApp < Rails::Application
+
+  config.root = File.dirname(__FILE__)
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  secrets.secret_token    = 'secret_token'
+  secrets.secret_key_base = 'secret_key_base'
+  config.eager_load = false
+
+  config.middleware.use Warden::Manager do |config|
+    Devise.warden_config = config
+  end
+
+  config.logger = Logger.new($stdout)
+  Rails.logger  = config.logger
+
+end
+
+Rails.application.initialize!
+
+DeviseCreateUsers.migrate(:up)
+
+class User < ActiveRecord::Base
+
+  devise :imap_authenticatable
+
 end

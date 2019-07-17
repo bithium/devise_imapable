0846e025cebe7f28de77d43414e123d61cd5e2f7
Filipe Alves <filipe.alves@bithium.com>
2019-07-17 11:57:59 +0100

    FIX: Rubocop warnings due to copyright header inclusion

--- lib/devise/imapable/version.rb
+++ lib/devise/imapable/version.rb
@@ -1,4 +1,5 @@
-#
+# frozen_string_literal: true
+
 # Copyright (C) 2019 Bithium S.A.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -17,8 +18,6 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# frozen_string_literal: true
 
 module Devise
   module Imapable
